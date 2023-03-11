eba560016d8495dc73804f257c2f82c0f09f2414
Satyam <satus2311@gmail.com>
2023-03-12 01:11:09 +0530

    Added flexGrid star animation

--- src/components/shows/ShowCard.jsx
+++ src/components/shows/ShowCard.jsx
@@ -1,10 +1,25 @@
+import { useRef } from 'react';
 import styled from 'styled-components';
 import { SearchImgWrapper, SearchCard } from '../common/SearchCard';
 import { StarIcon } from '../common/StarIcon';
 
 const ShowCard = ({ name, image, id, summary, onStarMeClick, isStarred }) => {
+  const starBtnRef = useRef();
+  const handleOnStarClick = () => {
+    onStarMeClick(id);
+
+    const starBtnEl = starBtnRef.current;
+
+    if (!starBtnEl) return;
+
+    if (isStarred) {
+      starBtnEl.classList.remove('animate');
+    } else {
+      starBtnEl.classList.add('animate ');
+    }
+  };
   const summarySliced = summary
-    ? summary.split(' ').slice(0, 10).join(' ').replace(/<.+?>/g, '')
+    ? summary.split(' ').slice(0, 10).join(' ').replace(/<.+?>/g, '') + ' ...'
     : 'No Description';
 
   return (
@@ -20,9 +35,8 @@ const ShowCard = ({ name, image, id, summary, onStarMeClick, isStarred }) => {
           Read more
         </a>
 
-        <StarBtn type='button' onClick={() => onStarMeClick(id)}>
+        <StarBtn ref={starBtnRef} type='button' onClick={handleOnStarClick}>
           <StarIcon active={isStarred} />
-          {/* {isStarred ? 'Unstar Me' : 'Star me'} */}
         </StarBtn>
       </ActionSection>
     </SearchCard>
@@ -57,5 +71,21 @@ const StarBtn = styled.button`
   align-items: center;
   &:hover {
     cursor: pointer;
+  }
+  &.animate {
+    ${StarIcon} {
+      animation: increase 0.75s ease-in forwards;
+      @keyframes increase {
+        0% {
+          transform: scale(1);
+        }
+        50% {
+          transform: scale(3) rotate(45deg);
+        }
+        100% {
+          transform: scale(1);
+        }
+      }
+    }
   }
 `;
